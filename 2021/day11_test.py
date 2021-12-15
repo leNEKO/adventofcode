9f4fd6fbdccce642293767cf79a9ab79ac387f73
Glinglin <le.neko@gmail.com>
2021-12-16 00:17:59 +0100

    day 11 with vizualization

--- 2021/day11_test.py
+++ 2021/day11_test.py
@@ -1,16 +1,21 @@
-from collections import namedtuple, defaultdict
+from collections import defaultdict, namedtuple
+from os import system
+from time import sleep
+
 from guts.loader import Loader
 
 Position = namedtuple('Position', ('x', 'y'))
 
 
 class Octopus:
+    MAP = '🌕🌖🌗🌘🌑🌑🌑🌑🌑🌓'
+
     def __init__(self, value):
         self._value = value
         self._has_flashed = False
 
     def __repr__(self):
-        return str(self._value)
+        return self.MAP[self._value] + '  '
 
     def set_neighboor(self, neighboor):
         self._neighboor = neighboor
@@ -44,13 +49,14 @@ class Grid(dict):
 
     def __init__(self, matrix):
         self.load(matrix)
+        self._counter = 0
 
     def __repr__(self):
         def display():
             y = 0
             for position, octopus in self.items():
                 if position.y != y:
-                    yield "\n"
+                    yield "\n\n"
                 yield str(octopus)
                 y = position.y
         return ''.join(
@@ -58,6 +64,7 @@ class Grid(dict):
         )
 
     def load(self, matrix):
+        # init grid
         for y, row in enumerate(matrix):
             for x, value in enumerate(row):
                 self.update(
@@ -67,6 +74,7 @@ class Grid(dict):
                         )
                     }
                 )
+        # set neighboorhood
         for position, octopus in self.items():
             octopus.set_neighboor(
                 [
@@ -81,13 +89,14 @@ class Grid(dict):
             )
 
     def step(self):
+        self._counter += 1
+
         for octopus in self.values():
             yield from octopus.step()
+
         for octopus in self.values():
             octopus.reset()
 
-        print(self)
-        print()
 
 class Solver:
     def step(self, matrix):
@@ -101,6 +110,17 @@ class Solver:
 
         for _ in range(steps):
             yield from grid.step()
+
+    def simultaneous(self, path):
+        grid = Grid(Loader(path).read())
+        while True:
+            r = sum(grid.step())
+            system('clear')
+            print(grid)
+            sleep(1/12)
+            if r >= len(grid):
+                return grid._counter
+
 
 def test_solver():
     solver = Solver()
@@ -133,6 +153,7 @@ def test_solver():
         '45654',
     ]
 
+
 def test_process():
     solver = Solver()
 
@@ -141,4 +162,10 @@ def test_process():
 
 
 if __name__ == '__main__':
-    test_process()
+    solver = Solver()
+    print(
+        sum(
+            solver.process('day11_input.txt', 100)
+        ),
+        solver.simultaneous('day11_input.txt')
+    )
