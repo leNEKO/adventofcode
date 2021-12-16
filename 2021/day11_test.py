3684075ab77453449498cda4dd64b9d68239b725
Glinglin <le.neko@gmail.com>
2021-12-16 01:52:37 +0100

    refacto

--- 2021/day11_test.py
+++ 2021/day11_test.py
@@ -1,42 +1,43 @@
-from collections import defaultdict, namedtuple
+from collections import namedtuple
 from os import system
 from time import sleep
-
 from guts.loader import Loader
 
 Position = namedtuple('Position', ('x', 'y'))
 
 
 class Octopus:
-    MAP = '🌕🌖🌗🌘🌑🌑🌑🌑🌑🌓'
+    DISPLAY_MAP = '🌕🌖🌗🌘🌑🌑🌑🌑🌑🌓'
 
     def __init__(self, value):
         self._value = value
         self._has_flashed = False
 
     def __repr__(self):
-        return self.MAP[self._value] + '  '
+        return self.DISPLAY_MAP[self._value]
 
-    def set_neighboor(self, neighboor):
-        self._neighboor = neighboor
+    def set_neighborhood(self, neighborhood):
+        self._neighborhood = neighborhood
 
     def reset(self):
         self._has_flashed = False
 
+    def flash(self):
+        self._value = 0
+        self._has_flashed = True
+        yield 1
+
+        for n in self._neighborhood:
+            yield from n.step()
+
     def step(self):
         if self._has_flashed:
-            return 0
+            return
 
         self._value += 1
 
         if self._value > 9:
-            self._value = 0
-            self._has_flashed = True
-            yield 1
-
-            for n in self._neighboor:
-                if n is not None:
-                    yield from n.step()
+            yield from self.flash()
 
 
 class Grid(dict):
@@ -49,6 +50,7 @@ class Grid(dict):
 
     def __init__(self, matrix):
         self.load(matrix)
+        self.setup()
         self._counter = 0
 
     def __repr__(self):
@@ -56,37 +58,51 @@ class Grid(dict):
             y = 0
             for position, octopus in self.items():
                 if position.y != y:
-                    yield "\n\n"
+                    yield "\n"
                 yield str(octopus)
                 y = position.y
+
         return ''.join(
             display()
         )
 
+    def __hash__(self):
+        return hash(str(self))
+
     def load(self, matrix):
         # init grid
-        for y, row in enumerate(matrix):
-            for x, value in enumerate(row):
-                self.update(
-                    {
-                        Position(x, y): Octopus(
-                            int(value)
-                        )
-                    }
-                )
-        # set neighboorhood
-        for position, octopus in self.items():
-            octopus.set_neighboor(
-                [
-                    self.get(
-                        Position(
-                            position.x + offset.x,
-                            position.y + offset.y,
-                        )
+        [
+            self.update(
+                {
+                    Position(x, y): Octopus(
+                        int(value)
                     )
-                    for offset in self.OFFSET_MAP
-                ]
+                }
             )
+            for y, row in enumerate(matrix)
+            for x, value in enumerate(row)
+        ]
+
+    def setup(self):
+        for position, octopus in self.items():
+            positions = [
+                Position(
+                    position.x + offset.x,
+                    position.y + offset.y,
+                )
+                for offset in self.OFFSET_MAP
+            ]
+
+            neighborhood = [
+                neighbor
+                for neighbor in (
+                    self.get(position)
+                    for position in positions
+                )
+                if neighbor is not None
+            ]
+
+            octopus.set_neighborhood(neighborhood)
 
     def step(self):
         self._counter += 1
@@ -101,71 +117,72 @@ class Grid(dict):
 class Solver:
     def step(self, matrix):
         grid = Grid(matrix)
-        r = sum(grid.step())
+        grid.step()
 
         return str(grid).split()
 
-    def process(self, path, steps):
+    def part_a(self, path, steps):
         grid = Grid(Loader(path).read())
 
         for _ in range(steps):
             yield from grid.step()
 
-    def simultaneous(self, path):
-        grid = Grid(Loader(path).read())
+    def part_b(self, path):
+        lines = Loader(path).read()
+        return self.simultaneous(lines)
+
+    def simultaneous(self, lines):
+        grid = Grid(lines)
+
+        hashes = set()
+        loop = 0
+
+        def loop_detect():
+            key = hash(grid)
+            if key in hashes:
+                return 1
+            hashes.add(key)
+            return 0
+
         while True:
-            r = sum(grid.step())
             system('clear')
+
+            r = sum(grid.step())
             print(grid)
-            sleep(1/12)
+
             if r >= len(grid):
                 return grid._counter
 
+            loop += loop_detect()
 
-def test_solver():
-    solver = Solver()
+            print(loop)
+            if loop > 9:
+                return 'infinite loop'
 
-    step0 = [
-        '11111',
-        '19991',
-        '19191',
-        '19991',
-        '11111',
-    ]
-
-    step1 = solver.step(step0)
-
-    assert step1 == [
-        '34543',
-        '40004',
-        '50005',
-        '40004',
-        '34543',
-    ]
-
-    step2 = solver.step(step1)
-
-    assert step2 == [
-        '45654',
-        '51115',
-        '61116',
-        '51115',
-        '45654',
-    ]
+            sleep(1/60)
 
 
 def test_process():
     solver = Solver()
 
-    r = solver.process('day11_input_test.txt', 100)
+    r = solver.part_a('day11_input_test.txt', 100)
     assert 1656 == sum(r)
 
 
 if __name__ == '__main__':
     solver = Solver()
+
+    def random_lines(w, h):
+        from random import choice
+        values = '0123456789'
+        for _ in range(h):
+            yield ''.join(
+                [
+                    choice(values)
+                    for _ in range(w)
+                ]
+            )
+
     print(
-        sum(
-            solver.process('day11_input.txt', 100)
-        ),
-        solver.simultaneous('day11_input.txt')
+        solver.simultaneous(random_lines(11, 11))
     )
